ece7a75f11127c6dacbe995dc6f7b404f48ba401
Moynul Islam <moynul2015@cseku.ac.bd>
2026-01-01 01:13:20 +0600

    feat(RP-5): set up routing with demo home and movies page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
 
 function App() {
 
   return (
-    <>
-      <h1>MovieHub</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/movies" element = {<Movies/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import PageLayout from '../components/PageLayout/PageLayout'
+
+function Home() {
+  
+
+  return (
+    <PageLayout>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+      <h1>Home page.</h1>
+    </PageLayout>
+  );
+}
+
+export default Home;
