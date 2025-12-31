5918e3df9e0fb85a6c0dd7da9e1232669ed70088
Moynul Islam <87566898+200215-Moynul-Islam@users.noreply.github.com>
2026-01-01 01:17:01 +0600

    feat(RP-5): set up routing with demo home and movies page (#4)

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageLayout from '../components/PageLayout/PageLayout'
+
+function Movies() {
+  return (
+    <PageLayout>
+      <h1>Movies page.</h1>
+    </PageLayout>
+  );
+}
+
+export default Movies;
+
