d53a7e540a1c0ba981ff7fbc5c1654efd9aad97a
Moynul Islam <87566898+200215-Moynul-Islam@users.noreply.github.com>
2026-01-01 00:54:48 +0600

    feat(RP-2): implement footer componet (#2)

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+function Footer(){
+    return (
+        <footer className="footer">
+            © 2025 MovieHub. All rights reserved.
+        </footer>
+    )
+}
+
+export default Footer;
