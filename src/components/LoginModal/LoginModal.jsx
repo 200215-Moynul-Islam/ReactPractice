ca28654a13a334cd5c149467f6ac6e8044cd3a90
Moynul Islam <moynul2015@cseku.ac.bd>
2026-01-07 01:10:58 +0600

    feat(RP-7): implement login modal

---
+++ src/components/LoginModal/LoginModal.jsx
@@ -0,0 +1,37 @@
+import Modal from "../Modal/Modal"
+import "./LoginModal.css"
+import LoginModalHeader from "./LoginModalHeader";
+import { useState } from "react";
+import LoginModalBody from "./LoginModalBody";
+import LoginModalFooter from "./LoginModalFooter"
+
+function LoginModal({isOpen, onClose}){
+    const [formValues, setFormValues] = useState({
+        emailOrUsername: "",
+        password: "",
+    });
+    const [errors, setErrors] = useState({
+        emailOrUsername: "",
+        password: "",
+    });
+
+    const handleChange = (field) => (e) => {
+        setFormValues((prev) => ({...prev, [field]: e.target.value}));
+    };
+
+    //Implement submit handler
+
+    if(!isOpen){
+        return null;
+    }
+    return (
+        <Modal isOpen={isOpen} onClose={onClose} className="login-modal">
+            <LoginModalHeader />
+            <LoginModalBody formValues={formValues} errors = {errors} onChange={handleChange} />
+            <LoginModalFooter />
+            <hr className="login-modal-bottom-strip" />
+        </Modal>
+    )
+}
+
+export default LoginModal;
