5a1c2e478c87c9c9aeb16dc8a9615570bdd0b017
Moynul Islam <moynul2015@cseku.ac.bd>
2026-01-09 03:17:09 +0600

    refactor(RP-9): change the style of the auth buttons feat(RP-9): integrate the login and singup modal with the auth buttons

--- src/components/Header/HeaderAuth.jsx
+++ src/components/Header/HeaderAuth.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./Header.css"
+import LoginModal from "../LoginModal/LoginModal";
+import SignupModal from "../SignupModal/SignupModal"
 
-function HeaderAuth(){
+function HeaderAuth({onAuthSuccess}){
+    const [activeModal, setActiveModal] = useState(null);
+
+    const openLoginModal = () => setActiveModal("login");
+    const openSignupModal = () => setActiveModal("signup");
+    const closeModal = () => setActiveModal(null);
+
     return (
         <div className="header-auth">
-            <button>Log in</button>
-            <button>Sign up</button>
+            <button className="login" onClick={openLoginModal}>Log In</button>
+            <button className="signup" onClick={openSignupModal}>Sign Up</button>
+
+            <LoginModal isOpen={activeModal === "login"} onClose={closeModal} onSwitchToSignup={openSignupModal} onLoginSuccess={onAuthSuccess}/>
+            <SignupModal isOpen={activeModal === "signup"} onClose={closeModal} onSwitchToLogin={openLoginModal} onSignupSuccess={onAuthSuccess}/>
         </div>
     );
 }
